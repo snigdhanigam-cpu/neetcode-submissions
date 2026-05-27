d20d654e8b432538c668b20ccfb470237216e994
Snigdha <snigdha.nigam@gwmail.gwu.edu>
2026-05-27 15:55:45 -0400

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int res=0;
+        for (int i=0; i<heights.size();i++)
+        {
+            for (int j =i+1; j<heights.size();j++)
+            {
+                int area; 
+                area=((j-i)* std::min(heights[i],heights[j]));
+                res=std::max(res, area);
+
+            }
+        }
+        return res;
+        
+    }
+};
