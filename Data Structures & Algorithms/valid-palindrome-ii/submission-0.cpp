2a61b24d1e8a47137939da2b078eda078133dee3
Snigdha <snigdha.nigam@gwmail.gwu.edu>
2026-06-05 12:24:55 -0400

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s, int l, int r){
+        while(l<r)
+        {
+            if(s[l]!=s[r]){
+                return false;
+            }
+            l++;
+            r--;
+            
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int n=s.size();
+        int l=0; int r=n-1;
+        while(l<r)
+        {
+            if(s[l]!=s[r])
+            {
+                
+                return isPalindrome(s,l+1,r) || isPalindrome(s,l,r-1);
+
+            }
+            l++; 
+            r--;
+        }
+        return true;
+
+    }
+};
